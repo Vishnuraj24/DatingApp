d6195e8687dfa93d235548855368c390b213f25b
Kandukuri Vishnuraj <k.vishnurajchary@gmail.com>
2025-03-08 12:24:42 +0530

    added datetime extention - 80

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingApp.API.Extensions
+{
+
+    public static class DateTimeExtensions
+    {
+
+        public static int CalculateAge(this DateOnly dob)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+            var age = today.Year - dob.Year;
+            if (dob > today.AddYears(-age)) age--;
+
+            return age;
+        }
+
+    }
+
+
+}
