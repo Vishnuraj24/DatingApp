74829533181f1876c1ea671ab2e41d9c408cda5f
Kandukuri Vishnuraj <k.vishnurajchary@gmail.com>
2025-03-08 23:24:06 +0530

    photo url mapping

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.DTOs
         public int Id { get; set; }
         public string? UserName { get; set; }
         public int Age { get; set; }
-        public string? PhotouRL { get; set; }
+        public string? PhotoUrl { get; set; }
         public string? KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,11 +14,35 @@ namespace DatingApp.API.Helpers
 
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
             CreateMap<Photo, PhotoDto>();
         }
 
     }
+
+    // 1. CreateMap<AppUser, MemberDto>()
+    // This line tells AutoMapper to create a mapping between the AppUser class (source) and the MemberDto class (destination).
+
+    // 2. .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+    // This part of the code is a custom mapping configuration for the PhotoUrl property in the MemberDto class. Here's how it works:
+
+    // d => d.PhotoUrl
+    // This specifies the destination property (PhotoUrl) in the MemberDto class that you want to map.
+
+    // o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url)
+    // This specifies the source of the value for the PhotoUrl property.
+
+    // s represents the source object (AppUser).
+
+    // s.Photos accesses the Photos property of the AppUser object, which is a list of Photo objects.
+
+    // FirstOrDefault(x => x.IsMain) finds the first photo in the Photos list where the IsMain property is true (the main photo).
+
+    // !.Url accesses the Url property of the main photo. The ! (null-forgiving operator) is used to tell the compiler that you are sure the result of FirstOrDefault will not be null. If no main photo is found, this could throw an exception at runtime.
+
+    // What This Does
+    // It maps the Url of the main photo (where IsMain is true) from the AppUser.Photos list to the PhotoUrl property in the MemberDto.
 
 
 }
